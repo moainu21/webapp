55c93a6e2b31357790cda3810b5913781ff535c3
moainu <O.taichi21@icloud.com>
2025-03-21 06:01:43 +0000

    thrid commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>試合スケジュール</title>
+    <link rel="stylesheet" href="css/index.css">
+
+</head>
+<body>
+    <header>
+        <h1>試合スケジュール管理システム</h1><br>
+    </header>
+    <hr>
+    <article>
+        <div class="side">
+            <button id="create">作成</button>
+            <button id="show">閲覧</button>
+        </div>
+        <div class="contents">
+            <!-- <a href="#">１年生</a> -->
+        </div>
+    </article>
+    <footer>
+        <p>&copy; 2025 試合スケジュール管理システム</p>
+    </footer>
+
+    <script src="{{ asset('js/index.js') }}"></script>
+</body>
+</html>
